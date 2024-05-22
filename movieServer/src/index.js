8a808510c827f1de9a94adf46392588cec077d31
vaibhav indure <vaibhav.indure21@pccoepune.org>
2024-05-22 10:43:33 +0530

    Update index.js

--- movieServer/src/index.js
+++ movieServer/src/index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(cors())
 
 try{
-    mongoose.connect("mongodb+srv://indurevaibhav9:Vaibhav*18@cluster0.0r2c9zd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    mongoose.connect("mongodb://127.0.0.1:27017/mydb3")
     console.log("data base conneced successfully")
 }catch(e){
     console.log("there is an erro while connecting with db ", e)
